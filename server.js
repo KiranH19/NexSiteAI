eee33bb1758b4bf6656080930dd590bb3f6b9efd
Kiran Handi <kiranhandi232@gmail.com>
2026-06-27 12:38:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const path = require('path');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
+
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 5000;
 
 // ─── Middleware ───────────────────────────────────────────────────────────────
